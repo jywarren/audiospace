bbfa38f4658ad2f99dfa104ae5f5b518ec878b61
Jeffrey Warren <jeff@unterbahn.com>
2020-11-16 13:41:07 -0500

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-    launch: {
-        headless: process.env.HEADLESS !== 'false',
-        slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
-        devtools: true
-    }
+  launch: {
+    headless: process.env.HEADLESS !== 'false',
+    slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
+    devtools: true
+  },
+  server: {
+    command: 'http-server',
+    port: 8080,
+  },
 } 
