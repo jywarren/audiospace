1f11593cf173903d01ebcac9fa704f987ec59fe8
Jeffrey Warren <jeff@unterbahn.com>
2021-01-25 11:07:23 -0500

    Update jest-puppeteer.config.js

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -2,6 +2,7 @@ module.exports = {
   launch: {
     headless: process.env.HEADLESS !== 'false',
     slowMo: process.env.SLOWMO ? process.env.SLOWMO : 0,
+    ignoreHTTPSErrors: true, // https://github.com/puppeteer/puppeteer/issues/1137#issuecomment-515821727
     args: ['--no-sandbox', '--disable-setuid-sandbox'], // https://github.com/puppeteer/puppeteer/blob/main/docs/troubleshooting.md#setting-up-chrome-linux-sandbox
     devtools: true
   },
